febc03342d229a2ed225d3f908f8683d8ea6dea4
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2022-03-08 10:19:32 -0300

    ng-test

---
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -0,0 +1,11 @@
+import { UniqueIdService } from './unique-id.service';
+
+describe(UniqueIdService.name, () => {
+    it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
+        const service = new UniqueIdService();
+        const id = service.generateUniqueIdWithPrefix('app');
+        expect(id).toContain('app-');
+    });
+});
+
+
