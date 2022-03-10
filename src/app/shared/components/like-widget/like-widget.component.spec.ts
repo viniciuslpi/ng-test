7ee7bd2f0cd25bbed64ee1cfe4346c396b5919e9
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2022-03-10 16:21:47 -0300

    launcher configs

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -12,7 +12,7 @@ describe(LikeWidgetComponent.name, () => {
             imports: [LikeWidgetModule]
         }).compileComponents();
 
-        fixture = TestBed.createComponent(LikeWidgetComponent);
+        fixture = TestBed.createComponent(LikeWidgetComponent); // declaracao para ser chamado antes de cada bloco ser executado
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
@@ -21,19 +21,19 @@ describe(LikeWidgetComponent.name, () => {
         expect(component).toBeTruthy();
     });
     
-    it(`Should auto generate ID during ngOninit `, () => {
+    it(`Should auto generate ID during ngOninitwhen (@Input id) is not assigned`, () => {
         fixture.detectChanges();
         expect(component.id).toBeTruthy();
     });
 
-    it(`Should NOT auto generate ID when id input property is missing`, () => {
+    it(`Should NOT auto-generate ID during ngOnInit when (@Input id) is assigned`, () => {
         const someId = 'someId';
         component.id = someId;
         fixture.detectChanges();
         expect(component.id).toBe(someId);
     });
 
-    it(`#${LikeWidgetComponent.prototype.like.name} should trigger emission when called`, () => {
+    it(`#${LikeWidgetComponent.prototype.like.name} should trigger (@Output liked) when called`, () => {
         spyOn(component.liked, 'emit') //tem que acionar o espião para ele conseguir capitar a chamada do emitter
         fixture.detectChanges(); // detecta a atribuição de valores 
         component.like(); // chama o componente
